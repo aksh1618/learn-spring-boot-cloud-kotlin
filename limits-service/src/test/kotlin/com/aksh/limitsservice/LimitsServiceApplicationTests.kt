ba7b8a47b8b7fc5f18a9c3aab8c76cd8782d3f36
aksh1618 <aakarshituppal@gmail.com>
2020-02-26 01:23:52 +0530

    Limits service initialized

---
+++ limits-service/src/test/kotlin/com/aksh/limitsservice/LimitsServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aksh.limitsservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
