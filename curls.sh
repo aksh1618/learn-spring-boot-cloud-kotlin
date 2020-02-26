cd8f8bd8116ec0b7620bc547a588d557f4cafa02
aksh1618 <aakarshituppal@gmail.com>
2020-02-27 01:54:09 +0530

    Currency exchange hardcoded logic

--- curls.sh
+++ curls.sh
@@ -5,3 +5,4 @@ curl -s localhost:8888/limits-service/default | fx .
 curl -s localhost:8888/limits-service/qa | fx .
 curl -s localhost:8080/limits | fx .
 curl -s localhost:8080/limits | fx .
+curl -s localhost:8000/from/a/to/b | fx .
--- currency-exchange-service/src/main/kotlin/com/aksh/currencyexchangeservice/CurrencyExchangeServiceApplication.kt
+++ currency-exchange-service/src/main/kotlin/com/aksh/currencyexchangeservice/CurrencyExchangeServiceApplication.kt
@@ -4,12 +4,31 @@ import org.springframework.boot.WebApplicationType
 import org.springframework.core.env.get
 import org.springframework.fu.kofu.application
 import org.springframework.fu.kofu.webmvc.webMvc
+import java.math.BigDecimal
 
 val app = application(WebApplicationType.SERVLET) {
+    beans { bean<ExchangeService>() }
     webMvc {
         port = Integer.parseInt(env["server.port"])
+        router {
+            val exchangeService = ref<ExchangeService>()
+            GET("/from/{from}/to/{to}") { request ->
+                ok().body(
+                        exchangeService.getExchangeRate(
+                                request.pathVariable("from"), request.pathVariable("to")
+                        )
+                )
+            }
+        }
+        converters { jackson() }
     }
 }
+
+class ExchangeService {
+    fun getExchangeRate(from: String, to: String) = ExchangeValue(from, to, BigDecimal.ONE)
+}
+
+data class ExchangeValue(val from: String, val to: String, val conversionMultiple: BigDecimal, val id: Long = 0L)
 
 fun main() {
     app.run()
