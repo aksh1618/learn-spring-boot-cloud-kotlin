001b8da84d3bce9e0e788717f9dddadd72395e7d
aksh1618 <aakarshituppal@gmail.com>
2020-02-27 00:34:46 +0530

    Fetch dev config instead of default

--- curls.sh
+++ curls.sh
@@ -4,3 +4,4 @@ curl -s localhost:8080/limits | fx .
 curl -s localhost:8888/limits-service/default | fx .
 curl -s localhost:8888/limits-service/qa | fx .
 curl -s localhost:8080/limits | fx .
+curl -s localhost:8080/limits | fx .
