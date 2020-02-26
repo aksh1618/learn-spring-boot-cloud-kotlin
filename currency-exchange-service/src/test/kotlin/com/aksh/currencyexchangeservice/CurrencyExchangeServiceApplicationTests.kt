e6d2617db6157a1fc89fbb11f1acf8d37d45c033
aksh1618 <aakarshituppal@gmail.com>
2020-02-27 00:43:18 +0530

    Currency exchange service initialized

---
+++ currency-exchange-service/src/test/kotlin/com/aksh/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aksh.currencyexchangeservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
