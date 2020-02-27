1cfe8584e66933dd49e9c34394747a66d568a533
aksh1618 <aakarshituppal@gmail.com>
2020-02-28 02:44:08 +0530

    Call exchange service for conversion multiple

--- curls.sh
+++ curls.sh
@@ -9,3 +9,5 @@ curl -s localhost:8000/from/a/to/b | fx .
 curl -i localhost:8000/from/a/to/b
 curl -s localhost:8000/from/USD/to/INR | fx .
 curl -s localhost:8100/from/USD/to/INR/quantity/1 | fx .
+curl -s localhost:8100/from/USD/to/INR/quantity/35 | fx .
+curl -is localhost:8100/from/USD/to/INR/quantity/35
--- currency-conversion-service/src/main/kotlin/com/aksh/currencyconversionservice/CurrencyConversionServiceApplication.kt
+++ currency-conversion-service/src/main/kotlin/com/aksh/currencyconversionservice/CurrencyConversionServiceApplication.kt
@@ -4,6 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.support.beans
 import org.springframework.core.env.get
+import org.springframework.http.HttpStatus
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.server.ResponseStatusException
 import org.springframework.web.servlet.function.RouterFunction
 import org.springframework.web.servlet.function.ServerResponse
 import org.springframework.web.servlet.function.router
@@ -15,36 +18,57 @@ class CurrencyConversionServiceApplication
 fun main(args: Array<String>) {
     runApplication<CurrencyConversionServiceApplication>(*args) {
         val beansInitializer = beans {
+            bean<ConversionService>()
             bean {
                 val port = env["server.port"]
-                getRouter(port ?: "8080")
+                getRouter(ref(), port ?: "8080")
             }
         }
         addInitializers(beansInitializer)
     }
 }
 
-private fun getRouter(port: String): RouterFunction<ServerResponse> = router {
+private fun getRouter(conversionService: ConversionService, myPort: String): RouterFunction<ServerResponse> = router {
     GET("from/{from}/to/{to}/quantity/{quantity}") { request ->
+        val (multiple, port) = conversionService.getConversionMultipleAndPort(
+                request.pathVariable("from"),
+                request.pathVariable("to")
+        )
         ok().headers { headers ->
-            headers["Port"] = port
+            headers["Port"] = port ?: myPort
         }.body(request.run {
             CurrencyConversionBean(
                     pathVariable("from"),
                     pathVariable("to"),
-                    BigDecimal.ONE,
+                    multiple,
                     BigDecimal(pathVariable("quantity")),
-                    BigDecimal.ONE
+                    BigDecimal(pathVariable("quantity")) * multiple
             )
         })
     }
+}
+
+class ConversionService {
+    fun getConversionMultipleAndPort(fromCurrency: String, toCurrency: String) = RestTemplate()
+            .getForEntity(
+                    "http://localhost:8000/from/{from}/to/{to}",
+                    CurrencyConversionBean::class.java,
+                    mapOf("from" to fromCurrency, "to" to toCurrency)
+            ).let {
+                Pair(
+                        it.body?.conversionMultiple ?: throw ResponseStatusException(
+                                HttpStatus.INTERNAL_SERVER_ERROR, "Couldn't connect to exchange service"
+                        ),
+                        it.headers["Port"]?.get(0)
+                )
+            }
 }
 
 data class CurrencyConversionBean(
         val from: String,
         val to: String,
         val conversionMultiple: BigDecimal,
-        val quantity: BigDecimal,
-        val convertedQuantity: BigDecimal,
+        val quantity: BigDecimal?,
+        val convertedQuantity: BigDecimal?,
         val id: Long = 0L
 )
